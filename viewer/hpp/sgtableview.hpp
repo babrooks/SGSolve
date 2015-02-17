2a476f5b05ee3340757f686b49a61106dfe9f861
Benjamin Brooks <benjaminbrooks@wireless-s1-no-150-27-185.uchicago.edu>
2015-02-17 10:22:28 -0600

    sdq

---
+++ viewer/hpp/sgtableview.hpp
@@ -0,0 +1,24 @@
+#ifndef SGTABLEVIEW_HPP
+#define SGTABLEVIEW_HPP
+
+#include <QtWidgets>
+#include <QAbstractItemView>
+
+class SGTableView : public QTableView
+{
+  Q_OBJECT
+
+public:
+  SGTableView ()
+  {
+    
+  }
+
+  QSize sizeHint() const
+  {
+    
+  }
+  
+}
+
+#endif
